77889dfcadae78150a2ab28b73697594898126b6
matheus-monteiro <mmonteiro100@yahoo.com>
2018-07-11 21:41:49 -0300

    check if two given line segments intersect

---
+++ Reference/geometry/InterSeg.cpp
@@ -0,0 +1,11 @@
+#define ll long long
+#define Pll complex<ll>
+#define Y imag()
+#define X real()
+
+bool intersec(Pll a, Pll b, Pll c, Pll d)
+{
+	if(cross(c - a, c - b) * cross(d - a, d - b) <= 0 and cross(a - c, a - d) * cross(b - c, b - d) <= 0)
+		return true;
+	return false;
+}
