2432fdcca72921426b4b72750bcf8140f4e98d38
matheus monteiro <mmonteiro100.mm@gmail.com>
2020-08-08 12:29:25 -0300

    update

--- CodCad/Hello CodCad/2623.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-
-using namespace std;
-
-int main(){
-  cout<<"Ola Mundo!" << endl;
-  return 0;
-}
---
+++ math/modular_arithmetic.cpp
@@ -0,0 +1,46 @@
+/*
+ *  @author: Matheus Monteiro Silveira
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const int MOD = 1000000007LL;
+
+int normalize(int x)
+{
+	x = x % MOD;
+	if(x < 0) x += MOD;
+	return x;
+}
+
+int add(int a, int b)
+{
+	return normalize(normalize(a) + normalize(b));
+}
+
+int prod(int a, int b)
+{
+	return normalize(normalize(a) * normalize(b));
+}
+
+int sub(int a, int b)
+{
+	return normalize(normalize(a) - normalize(b));
+}
+
+int expMod(int x, int e)
+{
+	int ans = 1;
+	while(e > 0)
+	{
+		if(e & 1LL) ans = prod(ans, x), e--;
+		else x = prod(x, x), e /= 2;
+	}
+	return normalize(ans);
+}
+
+int inv(int x)
+{
+	return expMod(x, MOD - 2);
+}
