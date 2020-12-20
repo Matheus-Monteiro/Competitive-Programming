a15b799ce126dde1d8c16c98de6684506748ed82
matheus monteiro <mmonteiro100.mm@gmail.com>
2020-12-20 07:42:57 -0300

    Add multiplicative inverse with extended euclidean

--- math/modular_arithmetic.cpp
+++ math/modular_arithmetic.cpp
@@ -44,3 +44,25 @@ int inv(int x)
 {
 	return expMod(x, MOD - 2);
 }
+
+
+int extended_euclidean(int a, int b, int& x, int& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = extended_euclidean(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+int inv(int a, int m) {
+	int x, y;
+	int g = extended_euclidean(a, m, x, y);
+	if (g != 1) return -1; // não tem inverso
+	return ((x % m) + m) % m;
+}
+ 
